b98ab1510b23da45b8b5a3f8c346614753a66e52
Fergus Rose <fjmrose@gmail.com>
2023-05-28 14:39:57 +1000

    fix last migration

--- prisma/migrations/20230528023047_add_track_type_and_remix_artists_and_change_track_to_artist_relationship_to_m:n/migration.sql
+++ prisma/migrations/20230528023047_add_track_type_and_remix_artists_and_change_track_to_artist_relationship_to_m:n/migration.sql
@@ -4,40 +4,15 @@
   - You are about to drop the column `artist_id` on the `Track` table. All the data in the column will be lost.
 
 */
+-- CreateEnum
+CREATE TYPE "TrackType" AS ENUM ('ORIGINAL', 'REMIX', 'EDIT', 'CLUB_MIX', 'RADIO_EDIT', 'BOOTLEG');
+
 -- DropForeignKey
 ALTER TABLE "Track" DROP CONSTRAINT "Track_artist_id_fkey";
 
 -- AlterTable
-ALTER TABLE "Track" DROP COLUMN "artist_id";
-
--- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "firstName" TEXT,
-    "lastName" TEXT,
-    "email" TEXT NOT NULL,
-    "role" TEXT NOT NULL DEFAULT 'user',
-    "picture" TEXT,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Absence" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "approved" BOOLEAN NOT NULL DEFAULT false,
-    "userId" INTEGER NOT NULL,
-    "reporterId" INTEGER NOT NULL,
-    "from" TIMESTAMP(3) NOT NULL,
-    "to" TIMESTAMP(3) NOT NULL,
-    "notes" TEXT,
-
-    CONSTRAINT "Absence_pkey" PRIMARY KEY ("id")
-);
+ALTER TABLE "Track" DROP COLUMN "artist_id",
+ADD COLUMN     "track_type" "TrackType" NOT NULL DEFAULT 'ORIGINAL';
 
 -- CreateTable
 CREATE TABLE "_artist" (
@@ -52,9 +27,6 @@ CREATE TABLE "_remix_artist" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- CreateIndex
 CREATE UNIQUE INDEX "_artist_AB_unique" ON "_artist"("A", "B");
 
 -- CreateIndex
@@ -65,12 +37,6 @@ CREATE UNIQUE INDEX "_remix_artist_AB_unique" ON "_remix_artist"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_remix_artist_B_index" ON "_remix_artist"("B");
-
--- AddForeignKey
-ALTER TABLE "Absence" ADD CONSTRAINT "Absence_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Absence" ADD CONSTRAINT "Absence_reporterId_fkey" FOREIGN KEY ("reporterId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_artist" ADD CONSTRAINT "_artist_A_fkey" FOREIGN KEY ("A") REFERENCES "Artist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
